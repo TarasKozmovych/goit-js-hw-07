fd4ebf0681c77dfef68a045933781d7e4eb354c2
Taras Kozmovych <taraskom0404@gmail.com>
2022-07-31 04:10:19 +0300

    task-2-complit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const ulGalleryRef = document.querySelector(".gallery");
+
+galleryItems.forEach(({ preview, original, description }) => {
+    const addGalleryItems = `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    ulGalleryRef.innerHTML += addGalleryItems;
+});
+const lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: "250",
+});
+
+console.log(galleryItems);
